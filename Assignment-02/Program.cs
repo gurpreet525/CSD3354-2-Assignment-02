e33e7accdb4d1ac24c94aa8e7a2d622e8ee7cc4f
visualstudio <visualstudio@122-PC39>
2019-03-08 18:14:14 -0500

    lab 1

--- Assignment-02/Program.cs
+++ Assignment-02/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+//Gurpreet Gurpreet(c0712759)    Arshdeep Singh(c0716342)
+//CSD3354 Section 2
+//Assignment 2
+//March 8,2019
 
-namespace Assignment_02
+namespace c0712759
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DelegateExercises delegateExercises = new DelegateExercises();
+            delegateExercises.Method2();
+
         }
     }
 }
+
+
+public class DelegateExercises
+{
+    public delegate void MyDelegate();
+    void Method1()
+    {
+        Console.WriteLine("Method1");
+        Console.ReadLine();
+    }
+    public void Method2()
+    {
+        MyDelegate myDelegate = new MyDelegate(Method1);
+        myDelegate();
+    }
+
+}
+
