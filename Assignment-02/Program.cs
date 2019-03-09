55bd98a59f21a8007e10daeb12340f2491c1310b
visualstudio <visualstudio@122-PC39>
2019-03-08 20:59:39 -0500

    lab15

--- Assignment-02/Program.cs
+++ Assignment-02/Program.cs
@@ -17,27 +17,33 @@ namespace c0712759
             public static void Main()
             {
                 DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
+                try
+                {
+                    delegateExercises.Method3();
+                    Console.ReadLine();
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine("Exception Occurred.");
+                    Console.ReadLine();
+
+                }
             }
         }
-        public delegate int MyDelegate(out int i);
+
+        public delegate void MyDelegate();
 
         public class DelegateExercises
         {
-            int Method1(out int i)
+            void Method1()
             {
-                i = 100;
-                System.Console.WriteLine("Method1 " + i);
-                return 0;
+                throw new System.Exception();
             }
+
             public void Method3()
             {
                 MyDelegate myDelegate = new MyDelegate(Method1);
-                MyDelegate myDelegate1 = null;
-                MyDelegate myDelegate2 = myDelegate + myDelegate1;
-                int intValue;
-                myDelegate2(out intValue);
+                myDelegate();
             }
         }
 
