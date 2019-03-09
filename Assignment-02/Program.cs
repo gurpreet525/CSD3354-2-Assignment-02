f8bcbaa722faeac1622f3fd5089b82276dfaa12f
visualstudio <visualstudio@122-PC39>
2019-03-08 20:39:08 -0500

    lab12

--- Assignment-02/Program.cs
+++ Assignment-02/Program.cs
@@ -10,14 +10,42 @@ using System.Threading.Tasks;
 
 namespace c0712759
 {
-    public delegate void MyDelegate();
-    
-    public class DelegateExercises
+    namespace DelegatesAndEvents
     {
-        void Method3()
+        using System;
+
+        delegate void ExampleDelegate(string xyz);
+
+        class Program
         {
-            System.Console.WriteLine(MyDelegate.ToString());
+            public static void Method1(string xyz)
+            {
+                Console.WriteLine(xyz + " Method1");
+            }
+
+            public static void Method2(string xyz)
+            {
+                Console.WriteLine(xyz + " Method2");
+            }
+
+            public static void Main()
+            {
+                ExampleDelegate ex1Delegate, ex2Delegate, ex3Delegate, myDelegate;
+
+                ex1Delegate = new ExampleDelegate(Method1);
+                ex2Delegate = new ExampleDelegate(Method2);
+                ex3Delegate = ex1Delegate + ex2Delegate;
+                myDelegate = ex1Delegate - ex2Delegate;
+                ex1Delegate("AAA");
+                ex2Delegate("BBB");
+                ex3Delegate("CCC");
+                myDelegate("DDD");
+                myDelegate = ex3Delegate - ex1Delegate;
+                myDelegate("EEE");
+                myDelegate = ex3Delegate - ex2Delegate;
+                myDelegate("FFF");
+                Console.ReadLine();
+            }
         }
     }
 }
-
