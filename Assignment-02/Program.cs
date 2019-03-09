705d9f2437b1de2383341248ca5ac259bcde92f2
visualstudio <visualstudio@122-PC39>
2019-03-08 20:43:42 -0500

    lab14

--- Assignment-02/Program.cs
+++ Assignment-02/Program.cs
@@ -21,26 +21,23 @@ namespace c0712759
                 Console.ReadLine();
             }
         }
-
         public delegate int MyDelegate(out int i);
 
         public class DelegateExercises
         {
             int Method1(out int i)
             {
-                System.Console.WriteLine("Method1");
-                i = 10;
+                i = 100;
+                System.Console.WriteLine("Method1 " + i);
                 return 0;
             }
-
             public void Method3()
             {
                 MyDelegate myDelegate = new MyDelegate(Method1);
-                MyDelegate myDelegate1 = new MyDelegate(Method1);
+                MyDelegate myDelegate1 = null;
                 MyDelegate myDelegate2 = myDelegate + myDelegate1;
                 int intValue;
                 myDelegate2(out intValue);
-
             }
         }
 
