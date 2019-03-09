9488d16245dd9fc4462f9be80ba2c142eafd87c8
visualstudio <visualstudio@122-PC39>
2019-03-08 20:29:41 -0500

    lab9

--- Assignment-02/Program.cs
+++ Assignment-02/Program.cs
@@ -12,32 +12,10 @@ namespace c0712759
 {
     namespace DelegatesAndEvents
     {
-        public class Program
-        {
-            public static void Main()
-            {
-                DelegateExercises delegateExercises = new DelegateExercises();
-                delegateExercises.Method3();
-                Console.ReadLine();
-            }
-        }
-        }
         public delegate void MyDelegate();
-
-        public class DelegateExercises
+        public class DelegateExercises : MyDelegate
         {
-            void Method1()
-            {
-                System.Console.WriteLine("Method1");
-            }
-            public void Method3()
-            {
-                MyDelegate myDelegate = new MyDelegate(Method1);
-                myDelegate();
-            }
         }
-
     }
-
-
+}
 
