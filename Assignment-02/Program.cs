2d5c525e4316f976b9f42fd6b7bae642dca2b8b6
visualstudio <visualstudio@122-PC39>
2019-03-08 19:47:01 -0500

    lab3

--- Assignment-02/Program.cs
+++ Assignment-02/Program.cs
@@ -26,7 +26,7 @@ namespace c0712759
 public class DelegateExercises
 {
     public delegate void MyDelegate();
-    void Method1()
+    void Method1(int i)
     {
         Console.WriteLine("Method1");
         Console.ReadLine();
@@ -34,7 +34,7 @@ public class DelegateExercises
     public void Method2()
     {
         MyDelegate myDelegate = new MyDelegate(Method1);
-        myDelegate(50);
+        myDelegate();
     }
 
 }
